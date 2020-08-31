a5c303714c656780410b69cab96bfb14b403b5ad
Mateusz Grabowski <mategra5@gmail.com>
2020-09-01 00:53:24 +0200

    added end menu

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,27 @@
   <div id="app">
     <StartMenu v-if="!gameOn" @startGame="gameOn = true" />
     <Game v-if="gameOn" canvasHeight="500" canvasWidth="1000" />
+    <EndMenu v-if="gameFinished" />
   </div>
 </template>
 
 <script>
 import Game from "./components/Game.vue";
 import StartMenu from "./components/StartMenu.vue";
+import EndMenu from "./components/EndMenu.vue";
 
 export default {
   name: "App",
   components: {
     Game,
     StartMenu,
+    EndMenu,
   },
 
   data() {
     return {
       gameOn: false,
+      gameFinished: false,
     };
   },
 };
@@ -38,5 +42,30 @@ body {
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+.menu {
+  height: 40vh;
+  width: 80vh;
+  background: black;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+}
+
+button {
+  width: 220px;
+  height: 70px;
+  border: 3px dashed white;
+  background: black;
+  color: white;
+  margin: 0 15px;
+  font-family: "Press Start 2P", cursive;
+  font-size: 17px;
+  cursor: pointer;
+  line-height: 25px;
+  outline: none;
 }
 </style>
---
+++ src/components/EndMenu.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="menu" v-if="!exit">
+      <p class="mainText">Koniec gry</p>
+      <div class="results">
+        <p>Twój wynik: 0</p>
+        <p>Najlepszy wynik: 0</p>
+      </div>
+      <button>ZAGRAJ PONOWNIE</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EndMenu",
+};
+</script>
+
+<style scoped>
+.results {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.results p {
+  margin: 5px 0;
+}
+
+.mainText {
+  font-size: 30px;
+}
+</style>
